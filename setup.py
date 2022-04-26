009bed6210def506acd0012b7934b4b72e23d775
dhodcz2 <dhodcz2@uic.edu>
2022-04-26 17:48:57 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='ValidateOSM',
+    description='extracting, comparing, and validating information from OSM',
+    author='Daniel Hodczak',
+    author_email='dhodcz2@uic.edu',
+)
