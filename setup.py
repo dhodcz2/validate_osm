d87636a4ddc6229ebcb9a35a818a24851e9000b3
dhodcz2 <dhodcz2@uic.edu>
2022-04-27 21:52:25 -0500

    :brain:

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-from distutils.core import setup
+from setuptools import find_packages
+from setuptools import setup
 
 setup(
     name='validate_osm',
     description='extracting, comparing, and validating information from OSM',
     author='Daniel Hodczak',
     author_email='dhodcz2@uic.edu',
-    packages=['validate_osm', 'streetview_height', 'validate_building_height']
+    packages=find_packages(),
+    python_requires='>=3.10',
+    # packages=['validate_osm', 'streetview_height', 'validate_building_height'],
+    # py_modules=
 )
