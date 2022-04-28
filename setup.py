1c5f75d117aac6103aba2ac5af89a9e06547383c
dhodcz2 <dhodcz2@uic.edu>
2022-04-27 21:39:55 -0500

    sweet_potato:

--- setup.py
+++ setup.py
@@ -5,4 +5,5 @@ setup(
     description='extracting, comparing, and validating information from OSM',
     author='Daniel Hodczak',
     author_email='dhodcz2@uic.edu',
+    packages=['validate_osm', 'streetview_height', 'validate_building_height']
 )
