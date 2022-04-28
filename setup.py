2307044843d5e7e23cb5c1207e7bbcafdba5a3af
dhodcz2 <dhodcz2@uic.edu>
2022-04-28 00:36:28 -0500

    wtf

--- setup.py
+++ setup.py
@@ -5,10 +5,13 @@ with open('requirements.txt') as f:
     lines = f.readlines()
     # install_requires = f.read()
 
+# git+https://github.com/pnnl/buildingid-py
+# git+https://github.com/pnnl/buildingid-py.git#egg=pnnl-buildingid
 install_requires = [
     line.replace('\n', '')
     for line in lines
 ]
+install_requires.append('pnnl-buildingid @ https://github.com/pnnl/buildingid-py')
 setup(
     name='validate_osm',
     description='extracting, comparing, and validating information from OSM',
