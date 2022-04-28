5fa5f81b18d376c685cda5af91c6414992b5b26c
dhodcz2 <dhodcz2@uic.edu>
2022-04-27 23:59:41 -0500

    a

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ install_requires = [
     for line in lines
     if not line.startswith('-e')
 ]
-install_requires.append('pnnl-buildingid')
+install_requires.append('pnnl-buildingid @ https://github.com/pnnl/buildingid-py.git#egg=pnnl-buildingid')
+# install_requires.append('pnnl-buildingid')
 dependency_links = [
     line.split(' ')[1].replace('\n', '')
     for line in lines
