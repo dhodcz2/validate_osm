00944a223b5692e9be1c1662a974ca67680172fd
dhodcz2 <dhodcz2@uic.edu>
2022-04-27 23:09:15 -0500

    :egg:

--- setup.py
+++ setup.py
@@ -2,7 +2,19 @@ from setuptools import find_packages
 from setuptools import setup
 
 with open('requirements.txt') as f:
-    install_requires = f.read()
+    lines = f.readlines()
+    # install_requires = f.read()
+
+install_requires = [
+    line.replace('\n', '')
+    for line in lines
+    if not line.startswith('-e')
+]
+dependency_links = [
+    line.split(' ')[1].replace('\n', '')
+    for line in lines
+    if line.startswith('-e')
+]
 
 setup(
     name='validate_osm',
@@ -11,5 +23,6 @@ setup(
     author_email='dhodcz2@uic.edu',
     packages=find_packages(),
     python_requires='>=3.10',
-    # install_requires=install_requires,
+    install_requires=install_requires,
+    dependency_links=dependency_links
 )
