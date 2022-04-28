98d0171d44a374aba0d1cc9c897fa472506a6b4f
dhodcz2 <dhodcz2@uic.edu>
2022-04-27 22:44:19 -0500

    arst

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 
 with open('requirements.txt') as f:
     install_requires = f.read()
+
 setup(
     name='validate_osm',
     description='extracting, comparing, and validating information from OSM',
@@ -10,5 +11,5 @@ setup(
     author_email='dhodcz2@uic.edu',
     packages=find_packages(),
     python_requires='>=3.10',
-    install_requires=install_requires,
+    # install_requires=install_requires,
 )
