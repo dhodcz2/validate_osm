819c51f609d985b542c96e298b4ea705cb0e5b85
dhodcz2 <dhodcz2@uic.edu>
2022-05-05 03:54:48 -0500

    ...

--- shadow/__init__.py
+++ shadow/__init__.py
@@ -1,1 +1,11 @@
+
+import warnings
+
+import numpy as np
+import pyximport
+warnings.filterwarnings('ignore', '.*deprecated NumPy.*')
+pyximport.install(
+    setup_args={'include_dirs': np.get_include(), },
+    reload_support=True,
+)
 from shadow.elevation_map import run
