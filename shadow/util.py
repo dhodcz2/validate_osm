ea115588ca252554e0265f82f06d3a74d24e6959
dhodcz2 <dhodcz2@uic.edu>
2022-06-02 19:07:51 -0500

    solved nodata problem

--- shadow/util.py
+++ shadow/util.py
@@ -1,36 +1,19 @@
-import pyproj
-import rasterio
-from rasterio.transform import Affine
-import functools
-import glob
+from pyproj import CRS
+import math
 import tempfile
 from typing import Iterator
 
-if True:
-    from shadow.cutil import (
-        xtiles_from_lons,
-        ytiles_from_lats,
-        lons_from_xtiles,
-        lats_from_ytiles,
-    )
-from geopandas import GeoDataFrame, GeoSeries
-from pandas import Series, DataFrame
+import rasterio
+import rasterio.crs
 
-import geopandas as gpd
-
-import skimage.io
 import concurrent.futures
 import os
 
-import pandas as pd
 import pyproj
 
-from shadow.cutil import deg2num, nums2degs
-import pygeos.creation
+from shadow.cutil import deg2num, num2deg
 import cv2
-import cython
 import numpy as np
-import spatialpandas.geometry
 
 import pyproj.aoi
 
@@ -50,7 +33,15 @@ def get_utm_from_lon_lat(lon: float, lat: float) -> pyproj.crs.CRS:
     return utm_crs
 
 
-def get_shadow_image(gw: float, gs: float, ge: float, gn: float, zoom: int, basedir: str) -> np.ndarray:
+def get_shadow_image(
+        gw: float,
+        gs: float,
+        ge: float,
+        gn: float,
+        zoom: int,
+        basedir: str,
+        threshold: float = 0.0,
+) -> np.ndarray:
     # Note: python 3.9 glob.glob() does not have kwarg root_dir
     tw, tn = deg2num(gw, gn, zoom, True)
     te, ts = deg2num(ge, gs, zoom, True)
@@ -74,23 +65,21 @@ def get_shadow_image(gw: float, gs: float, ge: float, gn: float, zoom: int, base
     xtiles = xtiles.astype('U10')
 
     ytiles = np.char.add(ytiles, '.png')
-    xtiles = np.char.add(xtiles, '/')
+    xtiles = np.char.add(xtiles, os.sep)
 
     ytiles = np.repeat(ytiles, c_tilecount)
     xtiles = np.tile(xtiles, r_tilecount)
 
     tiles = np.char.add(xtiles, ytiles)
-    tiles = set(tiles)
-
-    pathname = os.path.join(basedir, f'**/{zoom}/[0-9]*/[0-9]*.png')
-
-    # paths is not aligned with xtiles
-    paths: list[str] = [
+    directory = os.path.join(basedir, str(zoom)) + os.sep
+    directory = os.path.normpath(directory)
+    if not directory.endswith(os.sep):
+        directory += os.sep
+    relevant = np.char.add(directory, tiles)
+    paths = [
         path
-        for path in glob.iglob(pathname, recursive=True)
-        if path[
-           path.rfind('/', 0, path.rfind('/')) + 1::
-           ] in tiles
+        for path in relevant
+        if os.path.exists(path)
     ]
     images: Iterator[np.ndarray] = concurrent.futures.ThreadPoolExecutor().map(lambda p: cv2.imread(p)[:, :, 0], paths)
     partitions: Iterator[str] = (
@@ -98,12 +87,12 @@ def get_shadow_image(gw: float, gs: float, ge: float, gn: float, zoom: int, base
         for path in paths
     )
     partitions: Iterator[tuple[str, str, str]] = (
-        partition.rpartition('/')
+        partition.rpartition(os.sep)
         for partition in partitions
     )
     xtiles_ytiles: Iterator[tuple[int, int]] = (
         (
-            int(partition[0].rpartition('/')[2]),
+            int(partition[0].rpartition(os.sep)[2]),
             int(partition[2])
         )
         for partition in partitions
@@ -112,27 +101,49 @@ def get_shadow_image(gw: float, gs: float, ge: float, gn: float, zoom: int, base
     raster = np.zeros((256 * r_tilecount, 256 * c_tilecount), dtype=np.uint8)
     for (xtile, ytile), image in zip(xtiles_ytiles, images):
         raster[rslices[ytile], cslices[xtile]] = image
+    if threshold > 0:
+        cutoff = math.ceil(threshold * 255)
+        raster[0 <= raster < cutoff] = 0    # because -1 is currently our nodata
     return raster
 
 
-def get_raster_path(gw: float, gs: float, ge: float, gn: float, zoom: int, basedir: str, outdir: str = None) -> str:
-    image = get_shadow_image(gw, gs, ge, gn, zoom, basedir)
-    height, width = image.shape
+def get_raster_path(
+        gw: float,
+        gs: float,
+        ge: float,
+        gn: float,
+        zoom: int,
+        basedir: str,
+        threshold: float = 0.0,
+        outdir: str = None,
+) -> str:
     tw, tn = deg2num(gw, gn, zoom, True)
     te, ts = deg2num(ge, gs, zoom, True)
     te += 1
     ts += 1
+
     if outdir is None:
         outdir = tempfile.gettempdir()
     if not os.path.exists(outdir):
         os.makedirs(outdir)
     outpath = os.path.join(outdir, f'{zoom}_{tw}_{ts}_{te}_{tn}.tif')
 
-    resolution = (ge - gw) / width
-    transform = Affine.translation(
-        xoff=gw - resolution / 2,
-        yoff=gs - resolution / 2
-    ) * Affine.scale(resolution, resolution)
+    image = get_shadow_image(
+        gw,
+        gs,
+        ge,
+        gn,
+        zoom=zoom,
+        basedir=basedir,
+        threshold=threshold,
+    )
+    height, width = image.shape
+    # width, height = image.shape
+
+    gw, gs, = num2deg(tw, ts, zoom, True)
+    ge, gn = num2deg(te, tn, zoom, True)
+
+    transform = rasterio.transform.from_bounds(gw, gs, ge, gn, width, height)
 
     with rasterio.open(
             outpath,
@@ -141,13 +152,53 @@ def get_raster_path(gw: float, gs: float, ge: float, gn: float, zoom: int, based
             height=height,
             width=width,
             count=1,
-            dtype=image.dtype,
-            crs='+proj=latlong',
-            transform=transform
+            dtype=np.int16,
+            nodata=-1,
+            crs=4326,
+            transform=transform,
     ) as f:
         f.write(image, 1)
 
     return outpath
+
+def get_raster_affine(
+        gw: float,
+        gs: float,
+        ge: float,
+        gn: float,
+        zoom: int,
+        basedir: str,
+        threshold: float = 0.0,
+) -> tuple[np.ndarray, tuple]:
+    "Returns the array and the Affnie transformation"
+    tw, tn = deg2num(gw, gn, zoom, True)
+    te, ts = deg2num(ge, gs, zoom, True)
+    te += 1
+    ts += 1
+
+    # if outdir is None:
+    #     outdir = tempfile.gettempdir()
+    # if not os.path.exists(outdir):
+    #     os.makedirs(outdir)
+    # # outpath = os.path.join(outdir, f'{zoom}_{tw}_{ts}_{te}_{tn}.tif')
+
+    image = get_shadow_image(
+        gw,
+        gs,
+        ge,
+        gn,
+        zoom=zoom,
+        basedir=basedir,
+        threshold=threshold,
+    )
+    height, width = image.shape
+    gw, ts = num2deg(tw, ts, zoom, True)
+    ge, tn = num2deg(te, tn, zoom, True)
+
+    image = image / 255
+    transform = rasterio.transform.from_bounds(gw, gs, ge, gn, width, height)
+    return image, transform
+
 
 
 if __name__ == '__main__':
@@ -155,15 +206,8 @@ if __name__ == '__main__':
         *(40.702844950247666, -74.02244810805952)[::-1],
         *(40.78270102430847, -73.93524412566495)[::-1],
         16,
-        # '/home/arstneio/Downloads/shadows/nyc-sep-22/'
-        '/home/arstneio/Downloads/shadows/'
+        '/home/arstneio/Downloads/shadows/test/16-winter/'
     )
-    path = get_raster_path(
-        *(40.702844950247666, -74.02244810805952)[::-1],
-        *(40.78270102430847, -73.93524412566495)[::-1],
-        16,
-        # '/home/arstneio/Downloads/shadows/nyc-sep-22/'
-        '/home/arstneio/Downloads/shadows/'
-    )
+
     print()
     # cells = get_cells_from_tiles(tiles, os.path.join('/home/arstneio/Downloads/shadows/nyc-sep-22/', str(16)))
