31c30ab5ebadb329f0dcb3a8cf0b1681397002b5
dhodcz2 <dhodcz2@uic.edu>
2022-07-12 20:28:20 -0500

    Decompositions geoseries, tiles, and spaces now fully functional

--- pluscodes/util/tile_cpp.pyx
+++ pluscodes/util/tile_cpp.pyx
@@ -1,21 +1,11 @@
 from libc.stdlib cimport malloc, free
 import pygeos.geometry
-from typing import Hashable
-import itertools
 
 from cython cimport view
 import pygeos.geometry
-import cython
-from libc.string cimport strlen, strcpy
-import math
 
 import pandas as pd
 import pygeos.creation
-import shapely.geometry.base
-from cpython.object cimport PyObject
-from cpython.ref cimport Py_INCREF
-from typing import Optional
-import numpy
 from geopandas import GeoDataFrame, GeoSeries
 from typing import Union
 
@@ -23,8 +13,6 @@ import numpy as np
 cimport numpy as np
 np.import_array()
 cimport util.cfuncs as cfuncs
-cimport util.z
-
 
 cdef extern from '<util/globals.h>':
     const char* ALPHABET
@@ -73,127 +61,151 @@ for n in range(6):
     YSTEPS[n] = pow(GRID_ROWS, GRID_LENGTH - n)
 
 
-cdef class Decomposition:
+cdef struct Decomposition:
+    size_t n_tiles
+    unsigned char code_length
+    unsigned long* longs
+
+cdef Decomposition decompose(
+        geometry: pygeos.geometry.Geometry,
+        unsigned long w,
+        unsigned long s,
+        unsigned long e,
+        unsigned long n,
+        unsigned char code_length,
+):
     cdef :
-        pygeos.geometry.Geometry footprint
-        size_t n_tiles
-        unsigned char code_length
-        unsigned long w, s, e, n
+        unsigned char grid_length
+        unsigned long xstep, ystep, trim_lon, trim_lat
+        size_t xtiles, ytiles, xpoints, ypoints, n_tiles, i, j, k
+        unsigned long[:] lw, ls
+        double[:] fx, fy, fw_boad, fs_broad
+        np.ndarray[UINT8, ndim=1] intersects
         unsigned long* longs
+        double final_lon_precision, final_lat_precision
 
-    def __cinit__(
-            self,
-            geometry: pygeos.geometry.Geometry,
-            unsigned long w,
-            unsigned long s,
-            unsigned long e,
-            unsigned long n,
-    ):
-        self.footprint = footprint
-        self.code_length = PAIR_LENGTH
-        self.tile_count = 0
-        self.tiles = self.__tiles()
+    grid_length = code_length - PAIR_LENGTH
 
-    cdef unsigned long* __tiles(self):
-        grid_length = self.code_length - PAIR_LENGTH
+    xstep = XSTEPS[grid_length]
+    ystep = YSTEPS[grid_length]
 
-        xstep = XSTEPS[grid_length]
-        ystep = YSTEPS[grid_length]
 
-        trim_lon = TRIM_LONS[grid_length]
-        trim_lat = TRIM_LATS[grid_length]
-        final_lon_precision = FINAL_LON_PRECISIONS[grid_length]
-        final_lat_precision = FINAL_LAT_PRECISIONS[grid_length]
+    trim_lon = TRIM_LONS[grid_length]
+    trim_lat = TRIM_LATS[grid_length]
+    final_lon_precision = FINAL_LON_PRECISIONS[grid_length]
+    final_lat_precision = FINAL_LAT_PRECISIONS[grid_length]
 
-        xtiles = (e_bound // xstep) - (w_bound // xstep)
-        ytiles = (n_bound // ystep) - (s_bound // ystep)
+    xtiles = (e // xstep) - (w // xstep)
+    ytiles = (n // ystep) - (s // ystep)
 
-        xpoints = xtiles + 1
-        ypoints = ytiles + 1
+    xpoints = xtiles + 1
+    ypoints = ytiles + 1
 
-        w_bound += xstep
-        s_bound += ystep
-        e_bound += xstep
-        n_bound += ystep
+    # w += xstep
+    # s += ystep
+    # e += xstep
+    # n += ystep
+    #
+    lw = view.array(shape=(xpoints,), itemsize=sizeof(unsigned long), format='L')
+    ls = view.array(shape=(ypoints,), itemsize=sizeof(unsigned long), format='L')
+    fw = view.array(shape=(xpoints,), itemsize=sizeof(double), format='d')
+    fs = view.array(shape=(ypoints,), itemsize=sizeof(double), format='d')
 
-        lw = view.array(shape=(xpoints,), itemsize=sizeof(unsigned long), format='L')
-        ls = view.array(shape=(ypoints,), itemsize=sizeof(unsigned long), format='L')
-        fw = view.array(shape=(xpoints,), itemsize=sizeof(double), format='d')
-        fs = view.array(shape=(ypoints,), itemsize=sizeof(double), format='d')
+    for i in range(xpoints):
+        lw[i] = w + (i * xstep) + xstep
+        fw[i] = <double> (lw[i] // trim_lon) / final_lon_precision - MAX_LON
 
-        for i in range(xpoints):
-            lw[i] = w_bound + (i * xstep)
-            fw[i] = <double> (lw[i] // trim_lon) / final_lon_precision - MAX_LON
+    for i in range(ypoints):
+        ls[i] = s + (i * ystep) + ystep
+        fs[i] = <double> (ls[i] // trim_lat) / final_lat_precision - MAX_LAT
 
-        for i in range(ypoints):
-            ls[i] = s_bound + (i * ystep)
-            fs[i] = <double> (ls[i] // trim_lat) / final_lat_precision - MAX_LAT
+    coords = view.array(shape=(xpoints * ypoints, 2), itemsize=sizeof(double), format='d')
+    for i in range(xpoints):
+        for j in range(ypoints):
+            coords[i * ypoints + j, 0] = fw[i]
+            coords[i * ypoints + j, 1] = fs[j]
 
-        fw_broad = view.array(shape=(xpoints * ypoints,), itemsize=sizeof(double), format='d')
-        fs_broad = view.array(shape=(xpoints * ypoints,), itemsize=sizeof(double), format='d')
-        for i in range(xpoints):
-            for j in range(ypoints):
-                fw_broad[i * ypoints + j] = fw[i]
-                fs_broad[i * ypoints + j] = fs[j]
-
-        # TODO: If only we had a stable C API for PyGEOS, we could iterate and instantiate ephemeral Point objects
-        #   instead of needing to create a numpy array of PyGEOS objects.
-        points = pygeos.creation.points(np.asarray(fw_broad), np.asarray(fs_broad), )
-        intersects = (
-            pygeos.intersects(self.footprint, points)
-            .reshape(xpoints, ypoints)
-        )
-        intersects = (
-                intersects[:xtiles, :ytiles]
-                & intersects[1:xpoints, :ytiles]
-                & intersects[1:xpoints, 1:ypoints]
-                & intersects[:xtiles, 1:ypoints]
-        )
-        self.tile_count = intersects.sum()
-
-        if self.tile_count == 0:
-            if grid_length == GRID_LENGTH:
-                raise ValueError('Infeasible tile request')
+    # TODO: If only we had a stable C API for PyGEOS, we could iterate and instantiate ephemeral Point objects
+    #   instead of needing to create a numpy array of PyGEOS objects.
+    points = pygeos.creation.points(
+        np.asarray(coords[:, 0], dtype=np.float64),
+        np.asarray(coords[:, 1], dtype=np.float64),
+    )
+    intersects = pygeos.intersects(geometry, points)
+    n_tiles = 0
+    for i in range(xtiles):
+        for j in range(ytiles):
+            k = i * ypoints + j
+            if (
+                intersects[k]
+                and intersects[k + 1]
+                and intersects[k + ypoints + 1]
+                and intersects[k + ypoints]
+            ):
+                n_tiles += 1
+                intersects[k] = True
             else:
-                self.code_length += 1
-                return self.__tiles()
-        else:
-            longs = malloc(sizeof(unsigned long) * self.tile_count * 2)
-            n = 0
-            for i in range(xtiles):
-                for j in range(ytiles):
-                    if intersects[i, j]:
-                        self.tiles[n] = lw[i]
-                        self.tiles[n + 1] = ls[j]
-                        n += 2
-            return longs
+                intersects[k] = False
 
+    if n_tiles == 0:
+        # TODO: For now we are going to exclude absurdly small geometries
+        #   our approach scales well with building size, however including "buildings" somehow smaller than a bus stop
+        #   will generate many, many tiles for a very small area, stressing visualization
+        if code_length >= 11:
+            return Decomposition(n_tiles=0, code_length=code_length, longs=NULL)
 
+        if grid_length == GRID_LENGTH:
+            raise ValueError('Unable to decompose geometry into sufficiently small tiles')
+        return decompose(
+            geometry=geometry,
+            w=w,
+            s=s,
+            e=e,
+            n=n,
+            code_length=code_length + 1,
+        )
 
-    def __dealloc__(self):
-        free(self.tiles)
+    longs = <unsigned long *> malloc(sizeof(unsigned long) * n_tiles * 2)
+    k = 0
+    for i in range(xtiles):
+        for j in range(ytiles):
+            if intersects[i * ypoints + j]:
+                longs[k] = lw[i]
+                longs[k + 1] = ls[j]
+                k += 2
+
+    assert k == n_tiles * 2
+
+    return Decomposition(
+        n_tiles=n_tiles,
+        code_length=code_length,
+        longs=longs,
+    )
 
 cdef class Decompositions:
     cdef :
-        Decomposition* decompositions
+        Decomposition * _decompositions
 
-        size_t n_tiles, n_gdf
-        np.ndarray strings
+        size_t _n_tiles, _n_gdf
+        np.ndarray _strings
 
-        double[:, :] bound
-        unsigned char[:] code_length
-        unsigned long[:,:] tile
-        unsigned long[:] iloc
-        unsigned long[:] space
+        double[:, :] _bounds
+        unsigned char[:] _code_lengths
+        unsigned long[:,:] _longs
+        unsigned long[:] _iloc, _spaces, _left_bounds,
+        unsigned int[:] _repeats
 
     def __cinit__(self, gdf: Union[GeoDataFrame, GeoSeries]):
         cdef :
             Decomposition decomposition
+            unsigned long[:] left_bounds, iloc
+            unsigned char[:] code_lengths
+            unsigned long[:] lw, ls, le, ln
+            size_t i, j, k
 
 
-
-        self.n_gdf = len(gdf)
-        self.decompositions = malloc(self.len * sizeof(Decomposition))
+        self._n_gdf = len(gdf)
+        self._decompositions = <Decomposition *>malloc(self._n_gdf * sizeof(Decomposition))
 
         fw, fs, fe, fn = gdf.bounds.values.T
         lw = np.ndarray.astype((
@@ -209,57 +221,77 @@ cdef class Decompositions:
                 (fn + MAX_LAT) * FINAL_LAT_PRECISION
         ), dtype=np.uint64)
 
-        for i in range(self.len):
-            decomposition = Decomposition( gdf.geometry[i], lw[i], ls[i], le[i], ln[i], )
-            self.decompositions[i] = decomposition
-            self.n_tiles += decomposition.n_tiles
-            self.repeat[i] = decomposition.n_tiles
+        geometry = gdf.geometry.values.data
 
-        self.repeat = view.array(shape=(self.n_tiles,), itemsize=sizeof(unsigned long), format='L')
-        self.longs = view.array(shape=(self.n_tiles,2), itemsize=sizeof(unsigned long), format='L')
-        self.iloc = view.array(shape=(self.n_tiles,), itemsize=sizeof(unsigned long), format='L')
-        self.code_length = view.array(shape=(self.n_tiles,), itemsize=sizeof(unsigned char), format='B')
+        self._repeats = view.array(shape=(self._n_gdf,), itemsize=sizeof(unsigned int), format='I')
+        self._code_lengths = view.array(shape=(self._n_gdf,), itemsize=sizeof(unsigned char), format='B')
+        self._left_bounds = view.array(shape=(self._n_gdf,), itemsize=sizeof(unsigned long), format='L')
 
-        n = 0
-        for i in range(self.n_tiles):
-            decomposition = self.decompositions[i]
-            for j in range(decomposition.tile_count):
-                self.tile[n, 0] = decomposition.tiles[j]
-                self.tile[n, 1] = decomposition.tiles[j + 1]
-                self.code_length[n] = decomposition.code_length
-                self.iloc[n] = i
-                n += 1
-
-        self.strings = cfuncs.get_strings(self.longs[:, 0], self.longs[:, 1], self.code_lengths)
-        self.bounds = cfuncs.get_bounds(self.longs[:, 0], self.longs[:, 1], self.code_lengths)
         # TODO: Rather than using the ILOC, space needs to actually be determine with the Z-order curve.
-        self.space = self.iloc
+        self._spaces = view.array(shape=(self._n_gdf,), itemsize=sizeof(unsigned long), format='L')
+
+        for i in range(self._n_gdf):
+            decomposition = decompose(
+                geometry=geometry[i],
+                w=lw[i],
+                s=ls[i],
+                e=le[i],
+                n=ln[i],
+                code_length=PAIR_LENGTH,
+            )
+            self._decompositions[i] = decomposition
+            self._left_bounds[i] = self._n_tiles
+            self._n_tiles += decomposition.n_tiles
+            self._repeats[i] = decomposition.n_tiles
+            self._code_lengths[i] = decomposition.code_length
+            self._spaces[i] = i
+
+        self._longs = view.array(shape=(self._n_tiles, 2), itemsize=sizeof(unsigned long), format='L')
+        self._iloc = view.array(shape=(self._n_tiles,), itemsize=sizeof(unsigned long), format='L')
+        code_lengths = view.array(shape=(self._n_tiles,), itemsize=sizeof(unsigned char), format='B')
+
+        k = 0
+        for i in range(self._n_gdf):
+            decomposition = self._decompositions[i]
+            for j in range(0, decomposition.n_tiles * 2, 2):
+                self._longs[k, 0] = decomposition.longs[j]
+                self._longs[k, 1] = decomposition.longs[j + 1]
+                code_lengths[k] = decomposition.code_length
+                self._iloc[k] = i
+                k += 1
+
+        assert k == self._n_tiles
+
+        self._strings = cfuncs.get_strings(self._longs[:, 0], self._longs[:, 1], code_lengths)
+        self._bounds = cfuncs.get_bounds(self._longs[:, 0], self._longs[:, 1], code_lengths)
+
 
     def __dealloc__(self):
-        free(self.decompositions)
+        for i in range(self._n_gdf):
+            free(self._decompositions[i].longs)
+        free(self._decompositions)
 
 
     def geoseries(self) -> GeoSeries:
-        index = pd.MultiIndex.from_arrays(
-            (self.iloc, self.space, self.strings),
-            names=('iloc', 'space', 'tile')
-        )
-        data = pygeos.creation.box(self.bounds[:, 0], self.bounds[:, 1], self.bounds[:, 2], self.bounds[:, 3])
+        spaces = np.asarray(self._spaces).repeat(np.asarray(self._repeats))
+        index = pd.MultiIndex.from_arrays((
+            np.asarray(self._iloc), np.asarray(spaces), self._strings
+        ), names=('iloc', 'space', 'string'))
+        data = pygeos.creation.box(self._bounds[:, 0], self._bounds[:, 1], self._bounds[:, 2], self._bounds[:, 3])
         return GeoSeries(data=data, index=index, crs=4326)
 
+    def tiles(self) -> dict[str, str]:
+        return {
+            self._strings[j]: self._spaces[i]
+            for i in range(self._n_gdf)
+            for j in range(self._left_bounds[i], self._left_bounds[i] + self._repeats[i])
+        }
 
-
-    def sets(self) -> dict[Hashable, set]:
-        ...
-
-    def spaces(self) -> dict[Hashable, Hashable]:
-        ...
-
-    def tiles(self) -> dict[Hashable, Hashable]:
-        ...
-
-
-
-
-
-
+    def spaces(self) -> dict[str, set[str]]:
+        return {
+            self._spaces[i]: {
+                self._strings[j]
+                for j in range(self._left_bounds[i], self._left_bounds[i] + self._repeats[i])
+            }
+            for i in range(self._n_gdf)
+        }
