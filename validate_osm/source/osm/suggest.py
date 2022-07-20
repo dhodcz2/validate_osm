1faf84be6d924ee4a7dffd5dc849b600839a1979
dhodcz2 <dhodcz2@uic.edu>
2022-07-19 19:16:00 -0500

    current implementation is foobar because not all .poly files are available

--- validate_osm/source/osm/suggest.py
+++ validate_osm/source/osm/suggest.py
@@ -1,8 +1,21 @@
 import concurrent.futures
+import geopandas as gpd
+
+import io
+import inspect
+import itertools
+import os.path
+from pathlib import Path
+import functools
+
+import geopandas.base
+from geopandas import GeoDataFrame, GeoSeries
+from pandas import Series, DataFrame
+
 import shapely.geometry
 import re
 from functools import cached_property
-from typing import Iterator, Iterable
+from typing import Iterator, Iterable, Type
 
 import numpy as np
 import pandas as pd
@@ -17,54 +30,152 @@ from shapely.geometry import MultiPolygon, Polygon
 from shapely.geometry.base import BaseGeometry
 
 
-def __construct(text: str) -> MultiPolygon:
-    text = text.replace('\n', '')
-    matches = re.findall(r'(?<=\d)(.*?)END', text)
-    if not matches:
-        raise ValueError('No polygons found')
-    splits = list(map(str.split, matches))
-    lons = (np.array(split[::2], dtype=np.float64) for split in splits)
-    lats = (np.array(split[1::2], dtype=np.float64) for split in splits)
-    polygons = map(Polygon, map(zip, lons, lats))
-    multipolygon = MultiPolygon(polygons)
-    return multipolygon
+#
+# def __construct(text: str) -> MultiPolygon:
+#     text = text.replace('\n', '')
+#     matches = re.findall(r'(?<=\d)(.*?)END', text)
+#     if not matches:
+#         raise ValueError('No polygons found')
+#     splits = list(map(str.split, matches))
+#     lons = (np.array(split[::2], dtype=np.float64) for split in splits)
+#     lats = (np.array(split[1::2], dtype=np.float64) for split in splits)
+#     polygons = map(Polygon, map(zip, lons, lats))
+#     multipolygon = MultiPolygon(polygons)
+#     return multipolygon
+#
+#
+# # TODO: cache poly files so that future requests are not necessary
+# def construct(urls: Iterable[str]) -> Iterator[MultiPolygon]:
+#     session = requests.Session()
+#     threads = concurrent.futures.ThreadPoolExecutor()
+#     for response in threads.map(session.get, urls):
+#         try:
+#             response.raise_for_status()
+#         except requests.exceptions.HTTPError:
+#             yield None
+#         else:
+#             yield __construct(response.text)
 
 
-def construct(urls: Iterable[str]) -> Iterator[MultiPolygon]:
-    session = requests.Session()
-    threads = concurrent.futures.ThreadPoolExecutor()
-    for response in threads.map(session.get, urls):
-        try:
-            response.raise_for_status()
-        except requests.exceptions.HTTPError:
-            yield None
-        else:
-            yield __construct(response.text)
+class CallableConstructPoly:
+    def __init__(self):
+        self._directory = Path(inspect.getfile(self.__class__)).parent / 'poly'
+        self._directory.mkdir(exist_ok=True, parents=True)
+
+    @staticmethod
+    def __extract(file: str, response: requests.Response):
+        with open(file, 'w') as f:
+            try:
+                response.raise_for_status()
+            except requests.exceptions.HTTPError as e:
+                # f.write('')
+                raise
+            else:
+                f.write(response.text)
+
+    @staticmethod
+    def __transform(textio: io.TextIOWrapper) -> BaseGeometry:
+        text = textio.read().replace('\n', '')
+        if not text:
+            return shapely.geometry.Point()
+        matches = re.findall(r'(?<=\d)(.*?)END', text)
+        if not matches:
+            raise ValueError(f'No polygons found in {text}')
+        splits = list(map(str.split, matches))
+        lons = (np.array(split[::2], dtype=np.float64) for split in splits)
+        lats = (np.array(split[1::2], dtype=np.float64) for split in splits)
+        polygons = map(Polygon, map(zip, lons, lats))
+        multipolygon = MultiPolygon(polygons)
+        return multipolygon
+
+    def __call__(self, poly: np.ndarray | Iterable[str]) -> np.ndarray:
+        directory = self._directory
+        files = [
+            directory / poly.rpartition('/')[2]
+            for poly in poly
+        ]
+        compress = [
+            not file.exists()
+            for file in files
+        ]
+        session = requests.Session()
+        responses = concurrent.futures.ThreadPoolExecutor().map(session.get, itertools.compress(poly, compress))
+        with concurrent.futures.ThreadPoolExecutor() as threads:
+            threads.map(self.__extract, itertools.compress(files, compress), responses)
+        textio: Iterator = concurrent.futures.ThreadPoolExecutor().map(lambda file: open(file, 'r'), files)
+        geometry = np.fromiter(map(self.__transform, textio), dtype=object, count=len(files))
+        for obj in textio:
+            obj.close()
+        return geometry
 
 
-class LazyLoc(_LocIndexer):
+construct = CallableConstructPoly()
+
+
+class LazyGeoLoc(_LocIndexer):
+    # def __getitem__(self, item):
+    #
+    #     # Only loads whatever geometry is currently required
+    #     if isinstance(item, tuple):
+    #         loc, column = item
+    #         if isinstance(column, str) and column != 'geometry':
+    #             return super().__getitem__(item)
+    #     else:
+    #         loc = item
+    #     geometry: GeoSeries | None | BaseGeometry = super().__getitem__((loc, 'geometry'))
+    #
+    #     if issubclass(type(geometry), BaseGeometry):
+    #         ...
+    #     elif geometry is None:
+    #         poly = super().__getitem__((loc, 'poly'))
+    #         geometry, = construct(poly)
+    #         self.__setitem__((loc, 'geometry'), geometry)
+    #
+    #     elif issubclass(type(geometry), Series):
+    #         isna = geometry.isna()
+    #         if count := isna.sum():
+    #             loc = isna.index[isna]
+    #             poly = super().__getitem__((loc, 'poly'))
+    #             geometry: np.ndarray = np.fromiter(construct(poly), dtype=object, count=count)
+    #             self.__setitem__((loc, 'geometry'), geometry)
+    #
+    #     else:
+    #         raise TypeError(type(geometry))
+    #
+    #     return super().__getitem__(item)
+    #
+
     def __getitem__(self, item):
-        obj: GeoDataFrame = super().__getitem__(item)
-        loc = obj.geometry.isna()
-        if count := loc.sum():
-            loc = loc[loc]
-            geometry =np.fromiter(construct(obj.loc[loc.index, 'poly']), dtype=object, count=count)
-            self.obj.loc[loc.index, 'geometry'] = geometry
-        return super(LazyLoc, self).__getitem__(item)
+        result = super().__getitem__(item)
+        if result is None and item[1] == 'geometry':
+            raise NotImplementedError()
+        elif isinstance(result, (GeoRegions, GeoSeries)):
+            isna = result.geometry.isna()
+            if count := isna.sum():
+                loc = isna.index[isna]
+                poly = super().__getitem__((loc, 'poly'))
+                geometry: np.ndarray = np.fromiter(construct(poly), dtype=object, count=count)
+                self.__setitem__((loc, 'geometry'), geometry)
+        return super(LazyGeoLoc, self).__getitem__(item)
 
 
 class GeoRegions(GeoDataFrame):
 
     @property
-    def loc(self, *args, **kwargs) -> LazyLoc:
-        return LazyLoc('loc', self)
+    def loc(self, *args, **kwargs) -> LazyGeoLoc:
+        return LazyGeoLoc('loc', self)
 
-    def __init__(self, *args, **kwargs):
-        kwargs['geometry'] = np.full((len(kwargs['index'], )), None)
-        super().__init__(*args, **kwargs)
+    @property
+    def _constructor(self):
+        return GeoRegions
 
 
 class Suggest:
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, '_instance'):
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
     @cached_property
     def _continents(self) -> GeoRegions:
         _continents: list[Africa] = [
@@ -89,7 +200,8 @@ class Suggest:
         return GeoRegions({
             'pbf': pbf,
             'poly': poly,
-        }, index=index)
+            'geometry': None,
+        }, index=index, crs=4326)
 
     @cached_property
     def _regions(self) -> GeoRegions:
@@ -125,10 +237,26 @@ class Suggest:
         ], dtype=str, )
 
         index = pd.MultiIndex.from_arrays([continents, region], names=['continent', 'region'])
-        return GeoRegions({
+        result = GeoRegions({
             'pbf': pbf,
             'poly': poly,
-        }, index=index)
+            'geometry': None,
+        }, index=index, crs=4326)
+
+        # TODO: Perhaps this could be dynamically determined from geofabrik rather than hardcoded
+        result = result.drop('''
+        south-africa-and-lesotho
+        us-midwest
+        us-northeast
+        us-pacific
+        us-south
+        us-west
+        alps
+        britain-and-ireland
+        germany-austria-and-switzerland
+        '''.split(), errors='ignore', level='region')
+
+        return result
 
     @cached_property
     def _subregions(self) -> GeoRegions:
@@ -167,12 +295,14 @@ class Suggest:
         return GeoRegions({
             'pbf': pbf,
             'poly': poly,
-        }, index=index)
+            'geometry': None,
+        }, index=index, crs=4326)
 
     @cached_property
     def _cities(self) -> GeoRegions:
         c = pyrosm.data.sources.cities
         cities = np.array(list(c._sources.keys()), dtype=str, )
+        index = pd.Index(cities, name='city')
         pbf = np.array([
             city['url']
             for city in c._sources.values()
@@ -184,13 +314,12 @@ class Suggest:
         return GeoRegions({
             'pbf': pbf,
             'poly': poly,
-        }, index=cities)
+            'geometry': None,
+        }, index=index, crs=4326)
 
     def cities(self, bbox: BaseGeometry, *args, url=False) -> np.array:
         # It does not appear that there is a way to subquery cities
-        loc = self._cities.loc
-        cities = loc[:]
-        # cities = self._cities.loc[:]
+        cities = self._cities.loc[:]
         cities = cities[cities.intersects(bbox)]
         if url:
             return cities['pbf'].values
@@ -199,7 +328,7 @@ class Suggest:
 
     def regions(self, bbox: BaseGeometry, *args, url=False) -> np.array:
         continents: np.ndarray = self.continents(bbox)
-        regions: GeoDataFrame = self._regions.loc[idx[continents, :], :]
+        regions: GeoRegions = self._regions.loc[idx[continents, :], :]
         regions = regions[regions.intersects(bbox)]
         if url:
             return regions['pbf'].values
@@ -208,7 +337,7 @@ class Suggest:
 
     def subregions(self, bbox: BaseGeometry, *args, url=False) -> np.array:
         regions: np.ndarray = self.regions(bbox)
-        subregions: GeoDataFrame = self._subregions.loc[idx[regions, :], :]
+        subregions: GeoRegions = self._subregions.loc[idx[regions, :], :]
         subregions = subregions[subregions.intersects(bbox)]
         if url:
             return subregions['pbf'].values
@@ -226,25 +355,17 @@ class Suggest:
 
 suggest = Suggest()
 
-# TODO: Unfortunately there is nothing in thi pyrosm library to indicate a discrepancy
-#   between subregions and special sub regions which overlap
-#   so we must do some web scraping
 if __name__ == '__main__':
-    # chicago = shapely.geometry.box(41.878, -87.629, 41.902, -87.614)
-    # manhattan = shapely.geometry.box(40.878, -73.629, 40.902, -73.614)
-    # moscow = shapely.geometry.box(55.878, 37.629, 55.902, 37.614)
-    # kyiv = shapely.geometry.box(50.878, 30.629, 50.902, 30.614)
-    # berlin = shapely.geometry.box(52.878, 13.629, 52.902, 13.614)
-    # mogadishu = shapely.geometry.box(1.878, 45.629, 1.902, 45.614)
-
     chicago = shapely.geometry.box(-87.629, 41.878, -87.614, 41.902)
-    manhattan = shapely.geometry.box(-73.629, 40.878, -73.614, 40.902)
-    moscow = shapely.geometry.box(37.629, 55.878, 37.614, 55.902)
-    kyiv = shapely.geometry.box(30.629, 50.878, 30.614, 50.902)
-    berlin = shapely.geometry.box(13.629, 52.878, 13.614, 52.902)
+    montreal = shapely.geometry.box(-73.5, 45.5, -73.4, 45.6)
+    sao_paulo = shapely.geometry.box(-46.5, -23.5, -46.4, -23.4)
+    cape_town = shapely.geometry.box(-33.5, -18.5, -33.4, -18.4)
+    timbuktu = shapely.geometry.box(16.5, -4.5, 16.6, -4.4)
+    belize = shapely.geometry.box(-90.5, 17.5, -90.4, 17.6)
+    abuja = shapely.geometry.box(7.5, 9.5, 7.6, 9.6)
+    nairobi = shapely.geometry.box(-36.5, -1.5, -36.4, -1.4)
 
+    suggest.subregions(chicago)
     suggest.continents(chicago)
     suggest.regions(chicago)
     suggest.subregions(chicago)
-    suggest.cities(chicago)
-    print()
